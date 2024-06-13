5d628c3ccb2ac31f335e9c621acb34db9e87dd0d
sserezha <125725834+sserezha@users.noreply.github.com>
2024-06-13 17:36:03 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,243 @@
+	const XLSX = require("xlsx");
+	const fs = require('fs');
+	const express = require('express');
+	const app = express();
+	const bodyParser = require('body-parser');
+	const dotenv = require('dotenv').config();
+	app.use(bodyParser.json());
+	app.set('view engine','ejs');
+	app.use(express.static('public'));
+	app.use(express.urlencoded({extended: false}));
+
+	const MongoClient = require('mongodb').MongoClient;
+	const url = process.env.URL;
+	const mongoClient = new MongoClient(url, {
+		maxPoolSize: 100,
+		minPoolSize: 10,
+		maxIdleTimeMS: 30000,
+		waitQueueTimeoutMS: 5000
+	});
+	const PORT = process.env.PORT;
+	async function getOptions(option){
+		try{
+			await mongoClient.connect();
+			const db = mongoClient.db("main");
+			const collection = db.collection("options");
+			const gotOptions = await collection.find({optionName:option}).toArray();
+			if (gotOptions.length){
+				return gotOptions;
+			}else {
+				let errArray = [{"optionName":"err"}]; 
+			return errArray;
+			}
+		}catch(err){
+			let errArray = [{"optionName":"err"}]; 
+			console.log(err);
+			return errArray;
+		}finally{
+			await mongoClient.close();
+		}}
+	async function writeToDB(dataToWrite, dbname) {
+		try {
+			await mongoClient.connect();
+			const db = mongoClient.db("main");
+			const collection = db.collection(dbname);
+			const result = await collection.insertOne(dataToWrite);
+			console.log(result);
+			console.log(dataToWrite);
+		}catch(err) {
+			console.log(err);
+		} finally {
+			await mongoClient.close();
+		}
+	}//test
+	var mounths = new Map ([
+		['Январь','-01-'],
+		["Февраль",'-02-'],
+		['Март','-03-'],
+		['Апрель','-04-'],
+		['Май','-05-'],
+		['Июнь','-06-'],
+		['Июль','-07-'],
+		['Август','-08-'],
+		['Сентябрь','-09-'],
+		['Октябрь','-10-'],
+		['Ноябрь','-11-'],
+		['Декабрь','-12-'],
+	]);
+	let gotMounth = 'Февраль';
+	console.log(mounths.get(gotMounth));
+	//end of test
+
+	async function getFromDB(gotMounth,gotYear){
+		try {
+			let gotYear='2024';
+			let mounth = mounths.get(gotMounth);
+			await mongoClient.connect();
+			const db = mongoClient.db("main");
+			const collection = db.collection('registry');
+			const findResult = await collection.find({}).toArray();
+		var excellFile = XLSX.readFile('registry_.xlsx', {cellDates:true, cellStyles: true});
+		console.log(excellFile);
+			excellFile.Sheets.Лист1['!ref'] = 'A1:K200'
+			function checkDate(){
+				const result = [];
+				findResult.forEach((itemFirst) => {
+					if (itemFirst.enteredData.date.includes(mounth) && itemFirst.enteredData.date.includes(gotYear)){
+						result.push(itemFirst);
+						};
+				});
+				return result;
+				}
+			let foundResult = checkDate(findResult);
+			foundResult.forEach((item,index) =>{
+				let row = index+2;
+				let keys = ['A'+row,'B'+row, 'C'+row, 'D'+row, 'E'+row, 'F'+row, 'G'+row, 'H'+row];
+				keys.forEach((key,kIndex)=>{
+					let type='';
+					let val = '';
+					switch(kIndex){
+						case 0:
+						val = Date.parse((item.enteredData.date+'T00:00:00.000Z'));
+						type = 'd';
+						break
+						case 1:
+						val = item.enteredData.autoNo;
+						type = 's';
+						break
+						case 2:
+						val = item.enteredData.woodSpecies;
+						type = 's';
+						break
+						case 3:
+						val = item.enteredData.sortiment;
+						type = 's';
+						break
+						case 4:
+						if (item.enteredData.rideType === 1) {
+							val='Вывозка';
+						} else {val='Погрузка';}
+						type = 's';
+						break
+						case 5:
+						val = item.enteredData.rideFrom;
+						type = 's';
+						break
+						case 6:
+						val = item.enteredData.rideTo;
+						type = 's';
+						break
+						case 7:
+						val = item.enteredData.ridesCount;
+						type = 's';
+						break
+
+					}
+					if (type ==='d') {
+						excellFile.Sheets.Лист1[key] = {t:type, v:val, z:'dd/mm/yyyy'};
+						} else
+						{excellFile.Sheets.Лист1[key] = {t:type, v:val}}
+				});
+			});
+			let today = new Date()
+			today.toISOString().split('T')[0]
+			let fileName='downloadRegistry'+today.toISOString().split('T')[0]+'.xlsx';
+			XLSX.writeFile(excellFile, fileName, {cellDates:true, bookType:"xlsx"});
+			return fileName;
+			}catch(err) {
+			console.log(err);
+		} finally {
+
+			await mongoClient.close();
+		}
+	}
+	async function updateOptions(newOptions){
+		try{
+			console.log(newOptions.savedValues);
+			const toAdd = newOptions.savedValues;
+			await mongoClient.connect();
+			const db = mongoClient.db("main");
+			const collection = db.collection("options");
+			const result = await collection.updateOne({displayName:newOptions.displayName},{$set:{savedValues: toAdd}});
+			return result;
+		} catch(err){
+			console.log(err)
+		} finally{
+			await mongoClient.close();
+		}
+	}
+
+	// ____________ ALL GET IN HERE ____________
+	app.get('/', (req, res) => {
+		let failed = req.query.fail
+		res.render('index', {autoNumbers, failed})
+		
+	});
+	app.get('/?fail', (req, res) => {
+		let fail = req.query.fail;
+		res.render('indexs', {autoNumbers});
+		
+	});
+
+	app.get('/admin/', (req,res) =>{
+		res.render('admin',{action: req.params.action});
+	});
+
+	app.get('/admin/:action', (req,res) =>{
+		getOptions(req.params.action).then (result=>{
+			res.render('admin',{action: req.params.action, query: req.query, options: result[0]});
+		});
+		});
+
+	app.get('/success', (req,res) =>{
+		res.render('success');
+	});
+
+	// ____________ ALL POST IN HERE ____________
+	app.post('/updateOptions', (req, res) => {
+		const value = req.body;
+			updateOptions(value).then(res2 =>{
+				console.log("updated with "+ value["displayName"]);
+				res.json(value);
+			});
+			
+		});
+
+
+	app.post('/submit', (req,res) =>{
+		console.log(req.body);
+		let enteredData = req.body;
+		let database = 'registry';
+		console.log(enteredData);
+		console.log('~~~~~~~~~~~');
+		console.log(Object.keys(enteredData).length);
+		console.log('~~~~~~~~~~~');
+		if (Object.keys(enteredData).length != 8){
+			console.log('not inserted with error');
+			return res.redirect('/?fail=1');
+		}
+		
+		else {
+				writeToDB({enteredData}, database);
+				console.log('inserted');
+				return res.redirect('/success');
+		}
+	});
+
+	app.post('/download', (req,res) =>{
+		let toDownload = '';
+		console.log(req.body.mounth);
+		getFromDB(req.body.mounth).then(result=>{
+		console.log(result);
+		res.download(result);
+	});
+
+		});
+	// ____________ ALL OTHER IN HERE ____________
+	app.use((req, res, next) => {
+	res.render('404');
+	})
+
+	app.listen(PORT, () => {
+		console.log('Server is running');
+	})
